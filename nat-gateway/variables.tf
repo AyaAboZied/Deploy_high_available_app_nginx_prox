38dc98574b299502a571d142c9ef04ea1f544bdc
AyaAboZied <ayaabozaied94@gmail.com>
2023-02-05 09:25:38 +0200

    upload

---
+++ nat-gateway/variables.tf
@@ -0,0 +1,9 @@
+variable "nat_subnet_id" {
+}
+
+variable "nat_name" {
+  type = string
+}
+
+variable "nat_depends_on" {
+}
---
+++ subnet/variables.tf
@@ -0,0 +1,11 @@
+variable "subnet_cidr_block" {
+}
+
+variable "sub_vpc_id" {
+}
+
+variable "sub_availability_zone" {
+}
+
+variable "subnet_name" {
+}
